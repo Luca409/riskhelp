d9e831e5a12ea0c147731ca0ee328bad4c5cf16e
Luca Loncar <lucaloncar@Lucas-MacBook-Pro.local>
2016-11-08 14:59:03 -0500

    Major change, now using mongodb for storage

--- src/riskhelp/Main.java
+++ src/riskhelp/Main.java
@@ -12,95 +12,167 @@ import org.apache.http.message.BasicNameValuePair;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+import com.mongodb.Block;
+import com.mongodb.client.FindIterable;
+import static com.mongodb.client.model.Filters.*;
+import static com.mongodb.client.model.Sorts.ascending;
+import static java.util.Arrays.asList;
+import com.mongodb.BasicDBObject;
+
 
 public class Main {	
-	
-	public static final String ACCOUNT_SID = "";
-	public static final String AUTH_TOKEN = "";
-	
-	public static void main(String [] args) throws IOException{
-		
-		String gameNumber = "16913205"; //temporary variable; hard coded in game number
 
-		//holds last player found, so when player changes, text is sent
-		String lastPlayer = "";
-		
-		Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
-		
-		String[][] firstGame = {{"Luca409", "7326429205"}, 
-								{"McSpirittedAway", "7325352403"}, 
-								{"myeager", "7325818122"}, 
-								{"Ptamborino", "9083072256"},
-								{"ZachY8s", "7324398680"},
-								{"tommykirk", "9736507640"},
-								{"brettclarke", "7327708479"},
-								{"TheRyanator17", ""},
-								{"mmk150", ""},
-								{"alangou", "6097440734"},
-								{"cWizzle", "6165897822"},
-								{"LHBeers", "2694052260"}
-								};
-		
-		
+	public static final String ACCOUNT_SID = "";
+	public static final String AUTH_TOKEN = "";	
+
+	public static void main(String [] args) throws IOException{
 		//infinite while loop to make the application run indefinitely
 		while(true) {
-			
-			//run turn check every 5 minutes	
+			//run program every 5 minutes
 			try  {
-				System.out.println("sleep for 60 seconds");
+				System.out.println("sleep for 5 minutes");
 				Thread.sleep(1000 * 300);
 			} catch (Exception e) {
 			}
-			
-			String currentPlayer = getCurrentPlayer();
-			
-			System.out.println("Current player: " + currentPlayer);
-			
-			if(!currentPlayer.equals(lastPlayer)) {
-				
-				System.out.println("Current Player: " + currentPlayer + ", Last Player: " + lastPlayer);
-				
-				String number = "";
-				for(int i = 0; i < firstGame.length; i++){
-					if(currentPlayer.equals(firstGame[i][0])) {
-						number = firstGame[i][1];
-					}
-				}
-				
-				if(!number.equals("")) {
-					System.out.println("Hello " + currentPlayer + ", it's your turn in Game " + gameNumber + "!");;
-					//sendSMS(currentPlayer, number, gameNumber);
-				} else {
-					System.out.println("No number saved for player. No text message sent.");
-				}
-				
-				lastPlayer = currentPlayer;
+			checkAllGames();
+		}
+	}
+
+	private static void checkAllGames() throws IOException{
+		// ***** begin creating arraylist of gameNumber *****
+		MongoClient mongoClient = new MongoClient();
+		MongoDatabase db = mongoClient.getDatabase("riskhelp");
+		FindIterable<Document> iterable = db.getCollection("games").find(
+				new Document("status", "active"));
+		ArrayList<String> list = new ArrayList<String>();
+		iterable.forEach(new Block<Document>() {
+			@Override
+			public void apply(final Document document) {
+				list.add(document.getString("gameNumber"));
+				System.out.println(document);
 			}
-		}
-	}
-	
-	public static String getCurrentPlayer() throws IOException{
-		Document doc = Jsoup.connect("https://www.conquerclub.com/game.php?game=16913205").get();
+		});
+		// ***** end creating arraylist of gameNumber *******
 		
-		Element els = doc.getElementsByClass("status_green").first();
-		
-		if(els == null) {
-			els = doc.getElementsByClass("status_yellow").first();
+		//if there are no active games, kill program
+		if(list.size() == 0) {
+			System.out.println("There are no active games. Program killed.");
+			System.exit(1);
 		}
 		
-		String currentPlayer = els.text();
-		
-		currentPlayer = currentPlayer.substring(currentPlayer.indexOf(':') + 1, currentPlayer.length());
-		
-		return currentPlayer;
+		//check all the active game
+		for(int i = 0; i < list.size(); ++i) {
+			checkGame(list.get(i));
+		}
 	}
-	
-	public static void sendSMS(String playerName, String number,  String gameNumber) {
+
+	/*
+	 * Requires: gameNumber is a valid gameNumber which exists in the mongo database
+	 *           game is active
+	 * Effects: Checks to see if its a different player's turn in the game. If it is, change last
+	 * 			player and send a text message. */
+	private static void checkGame(String gameNumber) throws IOException{
+		//get last player and currentplayer
+		String lastPlayer = getLastPlayer(gameNumber);
+		String currentPlayer = Player.getCurrentPlayer(gameNumber);
+		
+		//if no player is found, set game to inactive and end function
+		if(currentPlayer == null) {
+			System.out.println("This game is being set to inactive.");
+			setGameInactive(gameNumber);
+			return;
+		}
+		
+		//print player for testing purposes
+		System.out.println("Current player: " + currentPlayer);
+
+		//send text message, or don't
+		if(!currentPlayer.equals(lastPlayer)) {
+			System.out.println("Current Player: " + currentPlayer + ", Last Player: " + lastPlayer);
+			String number = getNumber(currentPlayer);
+			if(!number.equals("")) {
+				System.out.println("Hello " + currentPlayer + 
+									", it's your turn in Game " + gameNumber + "!");;
+				sendSMS(currentPlayer, number, gameNumber);
+			} else {
+				System.out.println("No number saved for player. No text message sent.");
+			}
+			//change last player in database
+			setLastPlayer(gameNumber, currentPlayer);
+		}
+	}
+
+	/* Requires: None
+	 * Effects: Sends text message*/
+	private static void sendSMS(String playerName, String number,  String gameNumber) {
+		Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
 		Message message = Message.creator(new PhoneNumber("+1" + number),
-		        new PhoneNumber("+17342355035"), "Hello " + playerName + ", it's your turn in Game " + gameNumber + "!").create();
+				new PhoneNumber("+17342355035"), "Hello " + playerName + 
+				", it's your turn in Game " + gameNumber + "!").create();
+		System.out.println(message.getSid());
+	}
 
-		    System.out.println(message.getSid());
+	/* Requires: username is a valid username which is stored in the Mongo database
+	 * Effects: returns the phone number of username*/
+	private static String getNumber(String username) {
+		// ***** begin searching for number *****
+		MongoClient mongoClient = new MongoClient();
+		MongoDatabase db = mongoClient.getDatabase("riskhelp");
+		FindIterable<Document> iterable = db.getCollection("phoneNumbers").find(
+				new Document("username", username));
+		ArrayList<String> list = new ArrayList<String>();
+		iterable.forEach(new Block<Document>() {
+			@Override
+			public void apply(final Document document) {
+				list.add(document.getString("phoneNumber"));
+			}
+		});
+		// ***** end searching for number *****
+		//if no player was found in database, just say there's no number
+		if(list.size() == 0) {
+			return "";
+		}
+
+		return list.get(0);
+	}
+
+	/* Requires: gameNumber is a valid game stored in the database 
+	 * Effects: returns the lastPlayer stored in gameNumber*/
+	private static String getLastPlayer(String gameNumber){
+		// ***** begin searching for lastPlayer *****
+		MongoClient mongoClient = new MongoClient();
+		MongoDatabase db = mongoClient.getDatabase("riskhelp");
+		FindIterable<Document> iterable = db.getCollection("games").find(
+				new Document("gameNumber", gameNumber));
+		ArrayList<String> list = new ArrayList<String>();
+		iterable.forEach(new Block<Document>() {
+			@Override
+			public void apply(final Document document) {
+				list.add(document.getString("lastPlayer"));
+			}
+		});
+		// ***** end searching for lastPlayer *****
+
+		return list.get(0);
+	}
+
+	/* Requires: None
+	 * Effects: sets lastPlayer in database */
+	private static void setLastPlayer(String gameNumber, String playerName) {
+		MongoClient mongoClient = new MongoClient();
+		MongoDatabase db = mongoClient.getDatabase("riskhelp");
+		db.getCollection("games").updateOne(new Document("gameNumber", gameNumber),
+				new Document("$set", new Document("lastPlayer", playerName)));
 	}
 	
+	//sets game number to inactive when the game is over
+	private static void setGameInactive(String gameNumber) {
+		MongoClient mongoClient = new MongoClient();
+		MongoDatabase db = mongoClient.getDatabase("riskhelp");
+		db.getCollection("games").updateOne(new Document("gameNumber", gameNumber),
+				new Document("$set", new Document("status", "inactive")));
+	}
 }
-
--- src/riskhelp/Player.java
+++ src/riskhelp/Player.java
@@ -1,5 +1,11 @@
 package riskhelp;
 
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
 public class Player {
 	
 	private String username;
@@ -8,5 +14,25 @@ public class Player {
 	public Player(String playerUsername, String playerPhoneNumber) {
 		username = playerUsername;
 		phoneNumber = playerPhoneNumber;
+	}
+	
+	//needed to separate this out because jsoup was conflicting with bson
+	public static String getCurrentPlayer(String gameNumber) throws IOException{
+		Document doc = Jsoup.connect("https://www.conquerclub.com/game.php?game=" + gameNumber).get();
+		
+		Element els = doc.getElementsByClass("status_green").first();
+		
+		if(els == null) {
+			els = doc.getElementsByClass("status_yellow").first();
+			if(els == null) {
+				return null;
+			}
+		}
+		
+		String currentPlayer = els.text();
+		
+		currentPlayer = currentPlayer.substring(currentPlayer.indexOf(':') + 1, currentPlayer.length());
+		
+		return currentPlayer;
 	}
 }
