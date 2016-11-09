196ec35f6218586fc02fba1c8287cbaed25b1ee0
Luca Loncar <lucaloncar@0587395747.wireless.umich.net>
2016-11-09 16:47:02 -0500

    Program checks twice before setting game to inactive

--- src/riskhelp/Player.java
+++ src/riskhelp/Player.java
@@ -18,21 +18,35 @@ public class Player {
 	
 	//needed to separate this out because jsoup was conflicting with bson
 	public static String getCurrentPlayer(String gameNumber) throws IOException{
-		Document doc = Jsoup.connect("https://www.conquerclub.com/game.php?game=" + gameNumber).get();
-		
-		Element els = doc.getElementsByClass("status_green").first();
-		
-		if(els == null) {
-			els = doc.getElementsByClass("status_yellow").first();
+		boolean secondLoop = false;
+		while(true) {
+			Document doc = Jsoup.connect("https://www.conquerclub.com/game.php?game=" + gameNumber).get();
+			
+			Element els = doc.getElementsByClass("status_green").first();
+			
 			if(els == null) {
-				return null;
+				els = doc.getElementsByClass("status_yellow").first();
+				if(els == null) {
+					if(!secondLoop) {
+						try  {
+							System.out.println("Game may be set to inactive. Wait 5 minutes and"
+												+ "try again.");
+							Thread.sleep(1000 * 300);
+						} catch (Exception e) {
+						}
+						secondLoop = true;
+						continue;
+					} else {
+						return null;						
+					}
+				}
 			}
+			
+			String currentPlayer = els.text();
+			
+			currentPlayer = currentPlayer.substring(currentPlayer.indexOf(':') + 1, currentPlayer.length());
+			
+			return currentPlayer;
 		}
-		
-		String currentPlayer = els.text();
-		
-		currentPlayer = currentPlayer.substring(currentPlayer.indexOf(':') + 1, currentPlayer.length());
-		
-		return currentPlayer;
 	}
 }
